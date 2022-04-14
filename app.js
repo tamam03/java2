bb71206d2db981b4e53c7205f02504581d33af08
tamami higashino <tamamihigashino@tamaminoMacBook-Pro.local>
2022-04-14 11:07:28 +0900

    finish

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-// let hello = 'Hello World';
-// alert('Hello' + 'World');
-
-// let str1 = 'Hello';
-// let str2 = 'World!!';
-// alert(str1 + str2);
+let hello = 'Hello World';
+alert('Hello' + 'World');
+同じ
+let str1 = 'Hello';
+let str2 = 'World!!';
+alert(str1 + str2);
 
 
 
@@ -63,16 +63,16 @@
    
 
 
-//let orange = 100;
-//let apple = 120;
+let orange = 100;
+let apple = 120;
 
-//if(orange<apple){
-  //alert('みかんの値段がリンゴより安い');
-//}else if(orange == apple){
-  //alert('みかんとリンゴが同じ値段');
-//}else{
-  //alert('みかんの値段がリンゴより高い');
-//}
+if(orange<apple){
+  alert('みかんの値段がリンゴより安い');
+}else if(orange == apple){
+  alert('みかんとリンゴが同じ値段');
+}else{
+  alert('みかんの値段がリンゴより高い');
+}
 
 
 
@@ -82,16 +82,16 @@
   //処理
 //}
 
-// let max = 100;
-// let num = 1;
-// let count = 0;
+let max = 100;
+let num = 1;
+let count = 0;
 
-// while(num<max){
-  // num =num * 2;
-  // count = count + 1;
-// }
+while(num<max){
+  num =num * 2;
+  count = count + 1;
+}
 
-// alert('2をかけ続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+alert('2をかけ続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
 
 
 //do...while文
